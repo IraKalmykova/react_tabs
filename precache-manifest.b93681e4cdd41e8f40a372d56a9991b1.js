58759858408882201c0d042e984f5f75d39f06ba
Ira <kalmykova_ira@ukr.net>
2019-07-29 19:44:49 +0300

    Updates

---
+++ precache-manifest.b93681e4cdd41e8f40a372d56a9991b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5c504e36f2f062445b46eb5fa871fb",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "762212e9a44125fa9d67",
+    "url": "/react_tabs/static/css/main.806dba94.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/react_tabs/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "762212e9a44125fa9d67",
+    "url": "/react_tabs/static/js/main.151e80fc.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
